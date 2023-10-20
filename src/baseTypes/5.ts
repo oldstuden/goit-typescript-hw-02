b9f48b5c4680e22184a372c89c0c51c8e1a357b0
oldstuden <atikin3002@gmail.com>
2023-10-20 21:41:52 +0200

    finished task1

--- src/baseTypes/5.ts
+++ src/baseTypes/5.ts
@@ -2,6 +2,7 @@
   Як ви визначите змінну в TypeScript, яка може приймати рядок або число (union type)? 
   І так само визначте змінну, яка може приймати тільки одне з двох рядкових значень: 'enable' або 'disable' (literal type)?
 */
-
-let union;
-let literal;
+type Enable = 'enable';
+type Disable = 'disable';
+let union: number | string;
+let literal: Enable | Disable;
--- src/baseTypes/9.ts
+++ src/baseTypes/9.ts
@@ -1,8 +1,21 @@
 /*
   Створіть новий тип даних, який підходить для цих двох об'єктів.
 */
-
-const page1 = {
+type Page = {
+  title: string;
+  likes: number;
+  accounts: [...string[]];
+  status: string;
+};
+type Properties = {
+  createAt: Date;
+  updateAt: Date;
+};
+type Details = {
+  details: Properties;
+};
+type DataPage = Page & Details;
+const page1: DataPage = {
   title: 'The awesome page',
   likes: 100,
   accounts: ['Max', 'Anton', 'Nikita'],
@@ -10,14 +23,14 @@ const page1 = {
   details: {
     createAt: new Date('2021-01-01'),
     updateAt: new Date('2021-05-01'),
-  }
-}
+  },
+};
 
-const page2 = {
+const page2: Page = {
   title: 'Python or Js',
   likes: 5,
   accounts: ['Alex'],
   status: 'close',
-}
+};
 
 export {};
